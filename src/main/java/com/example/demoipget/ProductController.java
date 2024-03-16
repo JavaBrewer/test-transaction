ea1dba25a2de1af8d315faec0429c444df458e65
JavaBrewer <hks0658@gmail.com>
2024-03-16 18:33:51 +0900

    add product test

---
+++ src/main/java/com/example/demoipget/ProductController.java
@@ -0,0 +1,20 @@
+package com.example.demoipget;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("/products")
+    public ResponseEntity<String> createProduct(@RequestBody Product product) {
+        productService.saveProduct(product);
+        return ResponseEntity.ok("Product saved successfully!");
+    }
+}
---
+++ src/main/java/com/example/demoipget/ProductService.java
@@ -0,0 +1,17 @@
+package com.example.demoipget;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Transactional
+    public void saveProduct(Product product) {
+        productRepository.save(product);
+    }
+}
